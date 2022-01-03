131d9f8be149165e517f1a083c0e22165e33ffd4
papany2000 <papany2000@rambler.ru>
2022-01-03 23:52:09 +0300

    add test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -19,6 +19,13 @@ describe('AppController (e2e)', () => {
     return request(app.getHttpServer())
       .get('/products')
       .expect(200)
-      .expect([{ "image": "https://kartinkin.net/uploads/posts/2021-07/thumbs/1626217214_28-kartinkin-com-p-marinovannie-ovoshchi-yeda-krasivo-foto-33.jpg", "price": 400, "name": "Яблоки", "id": "61d20c6c0ec2fc35de51120e" }, { "image": "https://img.povar.ru/list/6d/77/d7/43/ostrie_marinovannie_ogurchiki-308112.JPG", "price": 300, "name": "Огурцы", "id": "61d211f26087f79f4a4a2b53" }, { "image": "https://proprikol.ru/wp-content/uploads/2020/08/kartinki-kivi-34.jpg", "price": 400, "name": "киви", "id": "61d2cf417f5a37973cbf38bd" }]);
+      .expect([
+        {
+          image: 'ttps://img.povar.ru/list/6d/77/d7/43/ostrie_marinovannie_ogurchiki-308112.JPG',
+          price: 300,
+          name: 'Огурцы',
+          id: '61d3614fe436fd7b6ed8a054'
+        }
+      ]);
   });
 });
